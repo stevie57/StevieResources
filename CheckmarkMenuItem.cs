79d468608c7c8bf9b94384e9a005377b170cc04e
Stevie Vu <67438646+stevie57@users.noreply.github.com>
2022-05-05 13:58:33 -0700

    Update CheckmarkMenuItem.cs

--- CheckmarkMenuItem.cs
+++ CheckmarkMenuItem.cs
@@ -2,41 +2,27 @@
 [InitializeOnLoad]
 public static class CheckmarkMenuItem
 {
-
-    private const string MENU_NAME = "Example/Toggle";
-
-    private static bool enabled_;
-    /// Called on load thanks to the InitializeOnLoad attribute
-    static CheckmarkMenuItem()
-    {
-        CheckmarkMenuItem.enabled_ = EditorPrefs.GetBool(CheckmarkMenuItem.MENU_NAME, false);
-
-        /// Delaying until first editor tick so that the menu
-        /// will be populated before setting check state, and
-        /// re-apply correct action
-        EditorApplication.delayCall += () => {
-            PerformAction(CheckmarkMenuItem.enabled_);
-        };
-    }
-
-    [MenuItem(CheckmarkMenuItem.MENU_NAME)]
-    private static void ToggleAction()
-    {
-
-        /// Toggling action
-        PerformAction(!CheckmarkMenuItem.enabled_);
-    }
-
-    public static void PerformAction(bool enabled)
-    {
-
-        /// Set checkmark on menu item
-        Menu.SetChecked(CheckmarkMenuItem.MENU_NAME, enabled);
-        /// Saving editor state
-        EditorPrefs.SetBool(CheckmarkMenuItem.MENU_NAME, enabled);
-
-        CheckmarkMenuItem.enabled_ = enabled;
-
-        /// Perform your logic here...
+     private const string MenuName = "My Menu Item";
+ 
+     public static bool isEnabled;
+ 
+     static MyMenu()
+     {
+         isEnabled = EditorPrefs.GetBool(MenuName, true);
+     }
+ 
+     [MenuItem(MenuName)]
+     private static void ToggleAction()
+     {
+         isEnabled = !isEnabled;
+         EditorPrefs.SetBool(MenuName, isEnabled);
+     }
+ 
+     [MenuItem(MenuName, true)]
+     private static bool ToggleActionValidate()
+     {
+         Menu.SetChecked(MenuName, isEnabled);
+         return true;
     }
 }
+
