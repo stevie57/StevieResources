af60cc61230ed0b00ba704825a0120991d3b7688
Stevie Vu <67438646+stevie57@users.noreply.github.com>
2022-06-17 18:01:08 -0700

    Update
    Removing old files

--- StateServiceSubscription/StartGameStateSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = " States/Start Game State")]
-public class StartGameStateSO : AGameApplicationState
-{
-
-}
